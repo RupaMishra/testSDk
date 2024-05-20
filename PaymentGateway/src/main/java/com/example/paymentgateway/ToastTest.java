57cc42e5f1ef92a03e14b0188a2d7121b5663f02
RupaMishra <rupamishraa2000@gmail.com>
2024-05-20 16:59:19 +0530

    init repository

---
+++ PaymentGateway/src/main/java/com/example/paymentgateway/ToastTest.java
@@ -0,0 +1,12 @@
+package com.example.paymentgateway;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastTest {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
